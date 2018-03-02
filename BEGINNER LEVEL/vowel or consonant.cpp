483cf1361d0fa0c09641a227b5a6ca0d88934666
ADHARSHVAISHAG <36984379+ADHARSHVAISHAG@users.noreply.github.com>
2018-03-02 14:43:15 +0530

    Create vowel or consonant.cpp

---
+++ BEGINNER LEVEL/vowel or consonant.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    char a;
+    cout<<"enter the value";
+    cin>>a;
+if (a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U')
+    {
+    cout<<"the letter is vowel";
+   }
+else 
+ {
+    cout<<"the letter is consonant";
+ }
+}
