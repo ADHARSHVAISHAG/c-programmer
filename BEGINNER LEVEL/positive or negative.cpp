1614ca68b979550b759bbf06839eff6b2eaf24f2
ADHARSHVAISHAG <36984379+ADHARSHVAISHAG@users.noreply.github.com>
2018-03-02 14:28:37 +0530

    Create positive or negative.cpp

---
+++ BEGINNER LEVEL/positive or negative.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enter the value";
+    cin>>a;
+if (a>1)
+    {
+    cout<<"the number is positive";
+    }
+else if (a==0)
+   {
+    cout<<"the number is zero";
+   }
+else
+   {
+    cout<<"the number is negative";
+   }
+    return 0;
+}
