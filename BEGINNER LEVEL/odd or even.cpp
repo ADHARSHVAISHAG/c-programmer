9e95bdf51be440dfe7eae59f3f151c25916dfe6f
ADHARSHVAISHAG <36984379+ADHARSHVAISHAG@users.noreply.github.com>
2018-03-02 14:36:21 +0530

    Create odd or even.cpp

---
+++ BEGINNER LEVEL/odd or even.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int a;
+    cout<<"enter the value";
+    cin>>a;
+if (a%2==0)
+   {
+    cout<<"the number is even";
+   }
+else 
+  {
+    cout<<"the number is odd";
+  }
+}
