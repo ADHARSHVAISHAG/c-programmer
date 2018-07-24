a8c55fe848ac37587b430d6b872597d65f842cbb
ADHARSHVAISHAG <36984379+ADHARSHVAISHAG@users.noreply.github.com>
2018-07-24 11:41:54 +0530

    Create fac.py

---
+++ BEGINNER LEVEL/fac.py
@@ -0,0 +1,10 @@
+a =int(input())
+fl = 1
+if a < 0:
+   print("Sorry")
+elif a == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,a + 1):
+       fl = fl*i
+   print("The factorial of",a,"is",factorial)
